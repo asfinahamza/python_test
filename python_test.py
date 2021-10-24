beaf0884a99a413028995001480d9d30b4364d6d
asfinahamza <asfinahamza@gmail.com>
2021-10-24 22:10:55 +0530

    added binary search

--- python_test.py
+++ python_test.py
@@ -3,15 +3,15 @@ ps=int(input("enter the number to be checked "))
 ar=[]
 for x in range(n):
     ar.append(int(input("enter value ")))
-    ar.sort()
+ar.sort()
 print(ar)
 def index(a,p):
     for y in range(len(a)):
-        if(a[y]==p):
-            print("index: ",y)
-        elif(a[y]>p):
+        # if(a[y]==p):
+        #     print("index: ",y)
+        if(a[y]>p):
             # a.insert(y,p)
-            print("possible index: ",y)
+            print("index: ",y)
             break
     # print(a)
 index(ar,ps)
